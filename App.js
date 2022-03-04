c4f4928cc7aa8922195029eb0947be0bdb0ce845
atenajoon <atenarazm@gmail.com>
2022-03-03 21:57:29 -0800

    setup navigation

--- App.js
+++ App.js
@@ -1,25 +1,65 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from "./screens/Login";
-import Pilots from "./screens/Pilots";
-import New from "./screens/New";
-import Favorite from "./screens/Favorite";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Login from './screens/Login';
+import Pilots from './screens/Pilots';
+import New from './screens/New';
+import Favorite from './screens/Favorite';
 
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+// ===================Stack=====================
+/**
+ * Pilot stack that is used for both guest and user tabs.
+ */
+function PilotsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Pilots" component={Pilots} />
+      <Stack.Screen name="New" component={New} />
+      <Stack.Screen name="Favorite" component={Favorite} />
+      <Stack.Screen name="Logout" component={Login} />
+    </Stack.Navigator>
+  );
+}
+// ===================Stack=====================
+
+// ===================TABs=====================
+/**
+ * Tab for guest. It consists of two items, pilots stack and login screen
+ */
+function GuestTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Pilots" component={PilotsStack} />
+      <Tab.Screen name="Log in" component={Login} />
+    </Tab.Navigator>
+  );
+}
+
+function UserTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Pilots" component={PilotsStack} />
+      <Tab.Screen name="New" component={Login} />
+      <Tab.Screen name="Fav" component={Login} />
+      <Tab.Screen name="Log Out" component={Login} />
+    </Tab.Navigator>
+  );
+}
+// ===================TABs=====================
+
+/**
+ * Parent component that contains tabs.
+ * It's expected to switch between them based on user's context
+ */
 export default function App() {
-
-
+  const isLoggedIn = false;
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Pilots" component={Pilots} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="New" component={New} />
-        <Stack.Screen name="Favorite" component={Favorite} />
-      </Stack.Navigator>
-      {/* Rest of your app code */}</NavigationContainer>
+      {isLoggedIn ? <UserTab /> : <GuestTab />}
+    </NavigationContainer>
   );
 }
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -7,23 +7,16 @@
 // Guest button: user's redux must be set to null,
 // meaning the user is not logged in
 
-
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-export default function Login({ navigation }) {
-  return(
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+export default function Login({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Hi From LoginScreen</Text>
-      <View style={{  flexDirection: "row", justifyContent: "space-between"}}>
-        <Button
-          title="User"
-          onPress={()=>navigation.navigate("Pilots")}
-        />
-          <Button
-          title="Guest"
-          onPress={()=>navigation.navigate("Login")}
-        />
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Button title="User" onPress={() => navigation.navigate('Pilots')} />
+        <Button title="Guest" onPress={() => navigation.navigate('Login')} />
         {/* <Button
           title="Go to Login Again!"
           onPress={()=>navigation.push("Login")}
@@ -34,5 +27,5 @@ export default function Login({ navigation }) {
         /> */}
       </View>
     </View>
-  )
+  );
 }
