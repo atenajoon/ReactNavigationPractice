4b3fd3faeec327cc079352c6afe261f3be1a20c3
atenajoon <atenarazm@gmail.com>
2022-03-04 17:11:42 -0800

    button titles changes to real names

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -14,14 +14,17 @@ export default function Login({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Hi From LoginScreen</Text>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-        <Button title="User" onPress={() => navigation.navigate('Pilots')} />
-        <Button title="Guest" onPress={() => navigation.navigate('Login')} />
+      <View style={{flexDirection: 'row'}}>
+        <Button title="Sign in" onPress={() => navigation.navigate('Pilots')} />
+        <Button
+          title="Login as Guest"
+          onPress={() => navigation.navigate('New')}
+        />
         {/* <Button
           title="Go to Login Again!"
           onPress={()=>navigation.push("Login")}
-        />       */}
-        {/* <Button
+        />      
+        <Button
           title="Go to First Page!"
           onPress={()=>navigation.popToTop()}
         /> */}
