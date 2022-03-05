fd19456fa719f6371d19eb67e23af78aa4ed18c3
kei95 <kei0613shine.star@gmail.com>
2022-03-04 20:39:23 -0800

    test commit

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from "./screens/Login";
-import Pilots from "./screens/Pilots";
-import New from "./screens/New";
-import Favorite from "./screens/Favorite";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './screens/Login';
+import Pilots from './screens/Pilots';
+import New from './screens/New';
+import Favorite from './screens/Favorite';
 
+// test
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -20,6 +19,7 @@ export default function App() {
         <Stack.Screen name="New" component={New} />
         <Stack.Screen name="Favorite" component={Favorite} />
       </Stack.Navigator>
-      {/* Rest of your app code */}</NavigationContainer>
+      {/* Rest of your app code */}
+    </NavigationContainer>
   );
 }
