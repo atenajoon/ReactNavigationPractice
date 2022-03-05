108a7994d4224afa73d52f12646e4d6ab6358110
atenajoon <atenarazm@gmail.com>
2022-03-04 17:18:00 -0800

    I don't know, everything has been changed!

--- App.js
+++ App.js
@@ -6,60 +6,134 @@ import Login from './screens/Login';
 import Pilots from './screens/Pilots';
 import New from './screens/New';
 import Favorite from './screens/Favorite';
+import {View, Text, Button} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-// ===================Stack=====================
+// ===================STACKs=====================
 /**
- * Pilot stack that is used for both guest and user tabs.
+ * GuestStack that is used for both guest and user tabs.
  */
-function PilotsStack() {
+function GuestStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Pilots" component={Pilots} />
+      {/* <Stack.Screen name="Logout" component={Login} />  //this should be a sceern */}
+    </Stack.Navigator>
+  );
+}
+//-----------------------------------------
+/**
+ * UserStack that is used only for user tab.
+ */
+function UserStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Pilots" component={Pilots} />
       <Stack.Screen name="New" component={New} />
       <Stack.Screen name="Favorite" component={Favorite} />
-      <Stack.Screen name="Logout" component={Login} />
+      {/* <Stack.Screen name="Logout" component={Login} />  //this should be a sceern */}
     </Stack.Navigator>
   );
 }
-// ===================Stack=====================
+
 
 // ===================TABs=====================
 /**
  * Tab for guest. It consists of two items, pilots stack and login screen
  */
-function GuestTab() {
+
+ function U_Pilots() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Pilots" component={PilotsStack} />
-      <Tab.Screen name="Log in" component={Login} />
+      <Tab.Screen name="Pilots" component={Pilots} />
+      <Tab.Screen name="New" component={New} />
+      <Tab.Screen name="Favorite" component={Favorite} />
+      <Stack.Screen name="Logout" component={Login} />
     </Tab.Navigator>
   );
 }
 
-function UserTab() {
+function UserTebs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Pilots" component={PilotsStack} />
-      <Tab.Screen name="New" component={Login} />
-      <Tab.Screen name="Fav" component={Login} />
-      <Tab.Screen name="Log Out" component={Login} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="U_Pilots"
+        component={U_Pilots}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function GuestTebs() {
+  return (
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name="Pilots" component={Pilots} />
+      <Stack.Screen name="Login" component={Login} />
     </Tab.Navigator>
   );
 }
-// ===================TABs=====================
-
+//-----------------------------------------
+/**
+ * Tab for user. It consists of four items, pilots, Favorite, New stacks and the Logout screen.
+ */
+// function UserTebs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Pilots" component={Pilots} />
+//       <Tab.Screen name="New" component={New} />
+//       <Tab.Screen name="Favorite" component={Favorite} />
+//       <Stack.Screen name="Logout" component={Login} />
+//     </Tab.Navigator>
+//   );
+// }
+//-----------------------------------------
 /**
  * Parent component that contains tabs.
  * It's expected to switch between them based on user's context
  */
 export default function App() {
   const isLoggedIn = false;
+  // const isLoggedIn = true;
   return (
     <NavigationContainer>
-      {isLoggedIn ? <UserTab /> : <GuestTab />}
+      {isLoggedIn ? <UserTebs /> : <GuestTebs />}
     </NavigationContainer>
   );
 }
+
+// ===================SCREENs=====================
+/**
+ * Login screen is for guest.
+ */
+
+/**
+ * Logout screen is for user.
+ */
+
+// function GuestPilots() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Pilots" component={Pilots} />
+//       <Stack.Screen name="Login" component={Login} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// function App() {
+//   // App =>> Login page
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="GuestPilots"
+//           component={GuestPilots}
+//           options={{headerShown: false}}
+//         />
+//         <Stack.Screen name="Login" component={Login} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
